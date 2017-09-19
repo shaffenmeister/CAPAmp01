1fd68265360cf1a0a406ce01ccbcd66707b7c7b0
Christoph Appel <ch-appel@gmx.net>
2017-09-19 17:22:17 +0200

    Cleanup.

--- CAPAmp01Control.ino
+++ CAPAmp01Control.ino
@@ -10,6 +10,7 @@
  */
 #include <U8g2lib.h>
 #include <mcp23s08.h>
+#include <EventManager.h>
 
 /*
  * Own sources
@@ -18,6 +19,7 @@
 #include "src/Biino/BiinoInput.h"
 #include "src/Biino/BiinoVolume.h"
 #include "src/Biino/BiinoChannel.h"
+#include "ir_codes_DNT_RC11.h"
 
 /*
  * Local functions
@@ -27,7 +29,6 @@ void pciSetup(byte);
 /*
  * Definitions of constants
  */
-
 const unsigned long BUTTON_TIMEOUT_MS = 750;
 
 const int PIN_CS_BIINO_VOL = SS; // 10, SS signal (PB2)
@@ -57,13 +58,24 @@ const int EE_BIINO_VOL_END = EE_BIINO_VOL_START + INPUT_CHANNEL_CNT - 1;
 const int ADDR_BIINO_VOL = 0x20;
 const int ADDR_BIINO_INP = 0x21;
 
-static struct {
-  bool last_state = LOW;
-  bool cur_state = LOW;
-  bool active = false;
-  unsigned long tstart_ms;
-  unsigned long tcur_ms;
-  } button_state[2];
+enum class user_event {
+  INVALID = -1,
+  AMP_POWER = 1,
+  AMP_MUTE = 2,
+  CHOOSE_INPUT_1 = 3,
+  CHOOSE_INPUT_2 = 4,
+  CHOOSE_INPUT_3 = 5,
+  VOL_UP = 6,
+  VOL_DOWN = 7
+};
+
+enum class sys_state {
+  INVALID = -1,
+  INITIAL = 1,
+  STANDBY = 2,
+  AMP_OFF = 3,
+  RUNNING = 4
+};
 
 /*
  * Global variables
@@ -71,6 +83,15 @@ static struct {
 IRrecv g_ir_receiver(PIN_IR_RCV);
 decode_results results;
 U8G2_SSD1306_128X64_NONAME_1_4W_HW_SPI u8g2(U8G2_R0, PIN_CS_DISP, PIN_DC_DISP, PIN_RESET_DISP);
+EventManager gMyEventManager;
+
+static struct {
+  bool last_state = LOW;
+  bool cur_state = LOW;
+  bool active = false;
+  unsigned long tstart_ms;
+  unsigned long tcur_ms;
+  } button_state[2];
 
 BiinoInput g_biino_input(0,(uint8_t)((1<<INPUT_CHANNEL_CNT) - 1),PIN_CS_BIINO_VOL,ADDR_BIINO_INP,EE_BIINO_CHAN);
 BiinoVolume g_biino_volume(0,PIN_CS_BIINO_VOL,ADDR_BIINO_VOL,EE_BIINO_VOL);
@@ -96,20 +117,20 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }  
 
-  Serial.println("Setting up...");  
-  Serial.println("Setting up Biino Input...");  
+//  Serial.println("Setting up...");  
+//  Serial.println("Setting up Biino Input...");  
   g_biino_input.Setup();
-  Serial.print("\tChannel mask: ");
-  Serial.println(g_biino_input.channel_mask,BIN);
-  Serial.print("\tExpected mask: ");
-  Serial.println((uint8_t)((1<<INPUT_CHANNEL_CNT) - 1),BIN);
-  Serial.print("\tCurrent channel: ");
-  Serial.println(g_biino_input.GetCurrentChannel(),BIN);
-  Serial.println("Setting up Biino Input...Done.");  
-
-  Serial.println("Setting up Biino Volume...");
+//  Serial.print("\tChannel mask: ");
+//  Serial.println(g_biino_input.channel_mask,BIN);
+//  Serial.print("\tExpected mask: ");
+//  Serial.println((uint8_t)((1<<INPUT_CHANNEL_CNT) - 1),BIN);
+//  Serial.print("\tCurrent channel: ");
+//  Serial.println(g_biino_input.GetCurrentChannel(),BIN);
+//  Serial.println("Setting up Biino Input...Done.");  
+//
+//  Serial.println("Setting up Biino Volume...");
   g_biino_volume.Setup();
-  Serial.println("Setting up Biino Volume...Done.");    
+//  Serial.println("Setting up Biino Volume...Done.");    
 
   // Setup digital I/O pin directions
   pinMode(PIN_CS_BIINO_VOL,OUTPUT);
@@ -154,21 +175,45 @@ volatile static int g_rs_cmd = 0;
 volatile static uint8_t g_cur_channel = 0xFF;
 volatile static int rot_count = 0;
 volatile static uint16_t pot_count = 0;
+static sys_state state = sys_state::INITIAL;
 
+/*
+ * Main loop
+ */
 void loop() {
-  char buffer[4];
-  uint8_t n,newvol;
   int rot = rot_count;
   uint16_t pot = pot_count;
   bool update_screen = false;
   unsigned long tcur_ms = 0;
-  
-  if (g_ir_receiver.decode(&results)) {
-    g_ir_receiver.resume();
-  }
+  unsigned long ir_last_cmd = 0;
+  unsigned long ir_tlast_ms = 0;
 
   tcur_ms = millis();
 
+  /*  
+   * Process IR commands
+   */
+  if (g_ir_receiver.decode(&results)) {
+    if(results.decode_type == NEC) {
+      if(results.bits == 32) {
+        // New command
+        ir_tlast_ms = tcur_ms;
+        ir_last_cmd = results.value;
+      }
+      else if(results.bits == 0) {
+        // Repetition of last command
+        if(ir_last_cmd != 0) {
+          ir_tlast_ms = tcur_ms;
+        }
+      }
+    }
+
+    g_ir_receiver.resume();
+  }
+
+  /*  
+   * Process button states
+   */
   noInterrupts();
     
     // PIN_ROT_BUTTON: Rotary encoder's push button
@@ -184,12 +229,32 @@ void loop() {
     if(button_state[1].active == true) {
       if((tcur_ms-button_state[1].tstart_ms) >= BUTTON_TIMEOUT_MS) {
         // Perform action, if user presses button longer than BUTTON_TIMEOUT_MS
+        // Short push: MUTE ON/OFF
+        // Long push: AMP ON/OFF
         rot_count = 0;
         button_state[1].active = false;
+        
       }
     }
 
   interrupts();  
+
+  switch(state)
+  {
+    case sys_state::INITIAL:
+      state = sys_state::RUNNING;
+      break;
+    case sys_state::STANDBY:
+      state = sys_state::RUNNING;    
+      break;
+    case sys_state::AMP_OFF:
+      state = sys_state::RUNNING;
+      break;
+    case sys_state::RUNNING:
+      break;
+  }
+  
+  gMyEventManager.processEvent();
   
   if( rot != rot_count)  {
     rot = rot_count;
@@ -202,6 +267,9 @@ void loop() {
     update_screen = true;
   }      
 
+/*
+ * Update display
+ */
   if(update_screen == true) {
     u8g2.firstPage();
     u8g2.setFont(u8g2_font_inb19_mf); //u8g2_font_ncenB14_tr);
@@ -215,64 +283,64 @@ void loop() {
     update_screen = false;    
   }
      
-  if(Serial.available() > 0)
-    {
-      g_rs_cmd = Serial.read();
-      g_cur_channel = g_biino_input.GetCurrentChannel();
-      
-      switch(g_rs_cmd) {
-        case '+':
-          if(g_biino_input.SelectNext() == EXIT_FAILURE) 
-            g_biino_input.SelectFirst();
-          Serial.print("Current channel (+): ");
-          Serial.print(g_biino_input.GetCurrentChannel(),BIN);       
-          Serial.print(" / ");
-          Serial.println(g_biino_input.GetCurrentChannelNo(),DEC);       
-          break;
-        case '-':
-          if(g_biino_input.SelectPrevious() == EXIT_FAILURE) 
-            g_biino_input.SelectLast();
-          Serial.print("Current channel (-): ");
-          Serial.print(g_biino_input.GetCurrentChannel(),BIN);       
-          Serial.print(" / ");
-          Serial.println(g_biino_input.GetCurrentChannelNo(),DEC);       
-          break;
-        case 'M':
-          if(digitalRead(PIN_AMP_MUTE) == 1)
-          {
-            mute_amp();
-          }
-          else
-          {
-            unmute_amp();  
-          }
-          
-          Serial.print("AMP status: ");
-          Serial.println(digitalRead(PIN_AMP_MUTE));
-          break;
-        case 'v':
-          n = Serial.readBytesUntil('#', buffer, 3);
-          buffer[n] = 0;
-           
-          Serial.print("Current volume: ");
-          Serial.println(g_biino_volume.GetVolume(),DEC); 
-          
- /*         if(g_biino_volume.IncVolume() == EXIT_FAILURE) {
-            Serial.println("Error setting volume");
-            g_biino_volume.SetVolume(0);
-          }
-          */
-          
-          newvol = atoi(buffer);  
-          g_biino_volume.SetVolume(newvol);
-
-          Serial.print("New volume: ");
-          Serial.println(g_biino_volume.GetVolume(),DEC); 
-          
-          break;  
-      } 
-     
-    }
+//  if(Serial.available() > 0)
+//    {
+//      g_rs_cmd = Serial.read();
+//      g_cur_channel = g_biino_input.GetCurrentChannel();
+//      
+//      switch(g_rs_cmd) {
+//        case '+':
+//          if(g_biino_input.SelectNext() == EXIT_FAILURE) 
+//            g_biino_input.SelectFirst();
+//          Serial.print("Current channel (+): ");
+//          Serial.print(g_biino_input.GetCurrentChannel(),BIN);       
+//          Serial.print(" / ");
+//          Serial.println(g_biino_input.GetCurrentChannelNo(),DEC);       
+//          break;
+//        case '-':
+//          if(g_biino_input.SelectPrevious() == EXIT_FAILURE) 
+//            g_biino_input.SelectLast();
+//          Serial.print("Current channel (-): ");
+//          Serial.print(g_biino_input.GetCurrentChannel(),BIN);       
+//          Serial.print(" / ");
+//          Serial.println(g_biino_input.GetCurrentChannelNo(),DEC);       
+//          break;
+//        case 'M':
+//          if(digitalRead(PIN_AMP_MUTE) == 1)
+//          {
+//            mute_amp();
+//          }
+//          else
+//          {
+//            unmute_amp();  
+//          }
+//          
+//          Serial.print("AMP status: ");
+//          Serial.println(digitalRead(PIN_AMP_MUTE));
+//          break;
+//        case 'v':
+//          n = Serial.readBytesUntil('#', buffer, 3);
+//          buffer[n] = 0;
+//           
+//          Serial.print("Current volume: ");
+//          Serial.println(g_biino_volume.GetVolume(),DEC); 
+//          
+// /*         if(g_biino_volume.IncVolume() == EXIT_FAILURE) {
+//            Serial.println("Error setting volume");
+//            g_biino_volume.SetVolume(0);
+//          }
+//          */
+//          
+//          newvol = atoi(buffer);  
+//          g_biino_volume.SetVolume(newvol);
+//
+//          Serial.print("New volume: ");
+//          Serial.println(g_biino_volume.GetVolume(),DEC); 
+//          
+//          break;  
+//      } 
+//     
+//    }
 }
 
 /*
@@ -303,7 +371,6 @@ void read_rotary(void)
   if (digitalRead(PIN_ROT_DIR)) rot_count--;
     else rot_count++;
 }
-
 
 /*
  * Port B PCINT (D8 to D13, PCINT0 to PCINT7, PB0 to PB7)
